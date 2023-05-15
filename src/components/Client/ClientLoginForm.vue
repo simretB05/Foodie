99e5ef021297c5bc199e9446eb1d24d3b8c4cbc4
simretB05 <happysimret@gmail.com>
2023-05-14 19:49:26 -0600

    deleted dist folder

--- src/components/Client/ClientLoginForm.vue
+++ src/components/Client/ClientLoginForm.vue
@@ -33,16 +33,12 @@ export default {
                 },
                 method: `POST`,
                 data: {
-                    // data values required to send a POST login
                     email: this.email_value,
                     password: this.pass_value,
                 }
-                // on a response, assign a variable to the value of a response  
             } ).then( ( response ) => {
-                //set Cookies with a value of response from axios POST method
                 Cookies.set( `token`, response[`data`][`token`])
                 Cookies.set( `client_id`, response[`data`][`client_id`] )
-                // changing page once successful
                 this.$router.push(`/`)
             } ).catch((error) =>{
                 error;
